2c4d227b1b116369e4c0630a9fb51fa2689fc5ba
RobertMilea23 <robertmilea2003@yahoo.com>
2024-04-02 22:05:37 +0300

    Update Register component's title

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -7,7 +7,7 @@ const Register = () => {
   return (
     <div>
     <div className='bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative'> 
-      <h1 className='text-4xl text-whitefont-bold text-center mb-6'>Register, Marina</h1>
+      <h1 className='text-4xl text-whitefont-bold text-center mb-6'>Register</h1>
       <form action=''>
         <div className='relative my-4'>
             <input type='email' className='block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer' placeholder=''/>
