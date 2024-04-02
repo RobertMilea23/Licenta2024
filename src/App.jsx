41f2b6664b06c4d20cfc7ffa068c528cab4c8fa9
RobertMilea23 <robertmilea2003@yahoo.com>
2024-04-02 20:51:52 +0300

    Add react-icons and react-router-dom dependencies login and register pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Login from './components/Login'
+import backgroundImage from './assets/COVER_IMAGE.jfif'; 
+import {Route, Routes} from 'react-router-dom'
+import Register from './components/Register'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    
+    <div className='text-white h-[100vh] flex justify-center items-center bg-cover' style={{ backgroundImage: `url(${backgroundImage})`  }}>
+        <Routes>
+          <Route path='login' element={ <Login />}/>
+          <Route path='register' element={ <Register />}/>
+        </Routes>
+    </div>
+    </>
+      
+
+    
   )
 }
 
